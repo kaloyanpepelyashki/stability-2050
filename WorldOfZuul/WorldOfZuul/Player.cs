8172f7ce9aed2615a9a6ee9b850675b80f66a97a
Kaloyan Pepelyashki <93147724+kaloyanpepelyashki@users.noreply.github.com>
2025-10-27 13:20:40 +0100

    Described the Player class

---
+++ WorldOfZuul/WorldOfZuul/Player.cs
@@ -0,0 +1,35 @@
+﻿namespace WorldOfZuul;
+/// <summary>
+/// This class represents the player the class holds information related to the player of the game.
+/// </summary>
+/// <remarks>
+/// The class is a singleton class, meaning it cannot be instantiated. The class a single instance.
+/// To get the instance of the class, the GetTurnCounter method must be used
+/// </remarks>
+public class Player
+{
+    private static Player _instance;
+    public string Name { get; set; }
+    public Inventory Inventory { get; set; }
+
+    private Player()
+    {
+        Inventory = Inventory.GetInstance();
+    }
+
+    public static Player GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Player();
+        }
+        
+        return _instance;
+    }
+
+    public void SetPlayerName(string name)
+    {
+        Name = name;
+    }
+
+}
